7aef7c9058cb34f95f4d0af6d96a13c5f13d00de
lukki15 <50550773+lukki15@users.noreply.github.com>
2023-11-19 22:35:30 +0100

    add responsive placeholders

--- lib/resources/pages/home_page.dart
+++ lib/resources/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:nylo_framework/nylo_framework.dart';
 import 'package:flutter/material.dart';
-import '/resources/widgets/logo_widget.dart';
-import '/resources/widgets/safearea_widget.dart';
+import '/resources/widgets/responsive.dart';
 
 class HomePage extends NyPage {
   static String path = '/home';
@@ -11,14 +10,85 @@ class HomePage extends NyPage {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello World".tr()),
-        centerTitle: true,
+    return Responsive(
+        mobile: _Mobile(), tablet: _Tablet(), desktop: _Desktop());
+  }
+}
+
+class _Mobile extends StatelessWidget {
+  const _Mobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return EmailScreen();
+  }
+}
+
+class _Tablet extends StatelessWidget {
+  const _Tablet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Expanded(
+        flex: 6,
+        child: ListOfEmails(),
       ),
-      body: SafeAreaWidget(
-        child: Center(child: Logo()),
+      Expanded(
+        flex: 9,
+        child: EmailScreen(),
+      )
+    ]);
+  }
+}
+
+class _Desktop extends StatelessWidget {
+  const _Desktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size _size = MediaQuery.of(context).size;
+
+    return Row(children: [
+      Expanded(
+        flex: _size.width > 1340 ? 2 : 4,
+        child: SideMenu(),
       ),
-    );
+      Expanded(
+        flex: _size.width > 1340 ? 3 : 5,
+        child: ListOfEmails(),
+      ),
+      Expanded(
+        flex: _size.width > 1340 ? 8 : 10,
+        child: EmailScreen(),
+      )
+    ]);
+  }
+}
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ListOfEmails extends StatelessWidget {
+  const ListOfEmails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class EmailScreen extends StatelessWidget {
+  const EmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
