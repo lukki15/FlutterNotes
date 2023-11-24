5bc188021a9b711012361194221924300e2182b2
lukki15 <50550773+lukki15@users.noreply.github.com>
2023-11-24 22:48:47 +0100

    feat: :recycle: update list card element and replace leading icon

--- lib/resources/components/email_card.dart
+++ lib/resources/components/email_card.dart
@@ -8,17 +8,34 @@ class EmailCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> date_split = note.date.split('.');
+    String date_day = date_split.first;
+    String date_month = date_split[1];
+    String date_year = date_split[2];
     return Center(
       child: Card(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             ListTile(
-              leading: Icon(Icons.check_box_outline_blank),
-              title: Text(note.title),
+              leading: Column(
+                children: [
+                  Text(
+                    date_day,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(date_month + "/" + date_year),
+                  // Text(note.time),
+                ],
+              ),
+              title: Text(
+                note.title,
+                maxLines: 1,
+                overflow: TextOverflow.clip,
+              ),
               subtitle: Text(
                 note.body,
-                overflow: TextOverflow.fade,
+                overflow: TextOverflow.ellipsis,
                 maxLines: 2,
               ),
             ),
